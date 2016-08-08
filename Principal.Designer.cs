a8e8dd82ed85c87c648cdb20fe08e9325f4f61ef
diegohaji <diegohaji@users.noreply.github.com>
2016-08-08 11:51:24 -0300

    Add files via upload

---
+++ Principal.Designer.cs
@@ -0,0 +1,256 @@
+﻿namespace ProjetoSistemaMaquiagem
+{
+    partial class Principal
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BotaoCadastroFuncionario = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.BotaoCadastroCliente = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.BotaoCadastroProdutos = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.BotaoLançamentoHorarios = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.BotaoLançamentoPagamentos = new System.Windows.Forms.Button();
+            this.BotaoLançamentoEstoque = new System.Windows.Forms.Button();
+            this.BotaoGerarRelatorio = new System.Windows.Forms.Button();
+            this.BotaoLançamentoServiços = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // BotaoCadastroFuncionario
+            // 
+            this.BotaoCadastroFuncionario.Location = new System.Drawing.Point(109, 95);
+            this.BotaoCadastroFuncionario.Name = "BotaoCadastroFuncionario";
+            this.BotaoCadastroFuncionario.Size = new System.Drawing.Size(75, 23);
+            this.BotaoCadastroFuncionario.TabIndex = 0;
+            this.BotaoCadastroFuncionario.Text = "Cadastrar";
+            this.BotaoCadastroFuncionario.UseVisualStyleBackColor = true;
+            this.BotaoCadastroFuncionario.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(30, 100);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(62, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Funcionario";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(30, 144);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(39, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Cliente";
+            // 
+            // BotaoCadastroCliente
+            // 
+            this.BotaoCadastroCliente.Location = new System.Drawing.Point(109, 134);
+            this.BotaoCadastroCliente.Name = "BotaoCadastroCliente";
+            this.BotaoCadastroCliente.Size = new System.Drawing.Size(75, 23);
+            this.BotaoCadastroCliente.TabIndex = 3;
+            this.BotaoCadastroCliente.Text = "Cadastrar";
+            this.BotaoCadastroCliente.UseVisualStyleBackColor = true;
+            this.BotaoCadastroCliente.Click += new System.EventHandler(this.BotaoCadastroCliente_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(30, 186);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(49, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Produtos";
+            // 
+            // BotaoCadastroProdutos
+            // 
+            this.BotaoCadastroProdutos.Location = new System.Drawing.Point(109, 176);
+            this.BotaoCadastroProdutos.Name = "BotaoCadastroProdutos";
+            this.BotaoCadastroProdutos.Size = new System.Drawing.Size(75, 23);
+            this.BotaoCadastroProdutos.TabIndex = 5;
+            this.BotaoCadastroProdutos.Text = "Cadastrar";
+            this.BotaoCadastroProdutos.UseVisualStyleBackColor = true;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(30, 226);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(48, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Serviços";
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(109, 216);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 7;
+            this.button4.Text = "Cadastrar";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(280, 100);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(46, 13);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Horários";
+            // 
+            // BotaoLançamentoHorarios
+            // 
+            this.BotaoLançamentoHorarios.Location = new System.Drawing.Point(352, 95);
+            this.BotaoLançamentoHorarios.Name = "BotaoLançamentoHorarios";
+            this.BotaoLançamentoHorarios.Size = new System.Drawing.Size(75, 23);
+            this.BotaoLançamentoHorarios.TabIndex = 9;
+            this.BotaoLançamentoHorarios.Text = "Lançar";
+            this.BotaoLançamentoHorarios.UseVisualStyleBackColor = true;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(280, 144);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(66, 13);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "Pagamentos";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(280, 186);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(46, 13);
+            this.label7.TabIndex = 11;
+            this.label7.Text = "Estoque";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(280, 226);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(54, 13);
+            this.label8.TabIndex = 12;
+            this.label8.Text = "Relatórios";
+            // 
+            // BotaoLançamentoPagamentos
+            // 
+            this.BotaoLançamentoPagamentos.Location = new System.Drawing.Point(352, 134);
+            this.BotaoLançamentoPagamentos.Name = "BotaoLançamentoPagamentos";
+            this.BotaoLançamentoPagamentos.Size = new System.Drawing.Size(75, 23);
+            this.BotaoLançamentoPagamentos.TabIndex = 13;
+            this.BotaoLançamentoPagamentos.Text = "Lançar";
+            this.BotaoLançamentoPagamentos.UseVisualStyleBackColor = true;
+            // 
+            // BotaoLançamentoEstoque
+            // 
+            this.BotaoLançamentoEstoque.Location = new System.Drawing.Point(352, 176);
+            this.BotaoLançamentoEstoque.Name = "BotaoLançamentoEstoque";
+            this.BotaoLançamentoEstoque.Size = new System.Drawing.Size(75, 23);
+            this.BotaoLançamentoEstoque.TabIndex = 14;
+            this.BotaoLançamentoEstoque.Text = "Lançar";
+            this.BotaoLançamentoEstoque.UseVisualStyleBackColor = true;
+            // 
+            // BotaoGerarRelatorio
+            // 
+            this.BotaoGerarRelatorio.Location = new System.Drawing.Point(352, 216);
+            this.BotaoGerarRelatorio.Name = "BotaoGerarRelatorio";
+            this.BotaoGerarRelatorio.Size = new System.Drawing.Size(75, 23);
+            this.BotaoGerarRelatorio.TabIndex = 15;
+            this.BotaoGerarRelatorio.Text = "Verificar";
+            this.BotaoGerarRelatorio.UseVisualStyleBackColor = true;
+            // 
+            // BotaoLançamentoServiços
+            // 
+            this.BotaoLançamentoServiços.Location = new System.Drawing.Point(109, 254);
+            this.BotaoLançamentoServiços.Name = "BotaoLançamentoServiços";
+            this.BotaoLançamentoServiços.Size = new System.Drawing.Size(75, 23);
+            this.BotaoLançamentoServiços.TabIndex = 16;
+            this.BotaoLançamentoServiços.Text = "Lançar";
+            this.BotaoLançamentoServiços.UseVisualStyleBackColor = true;
+            // 
+            // Principal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(484, 354);
+            this.Controls.Add(this.BotaoLançamentoServiços);
+            this.Controls.Add(this.BotaoGerarRelatorio);
+            this.Controls.Add(this.BotaoLançamentoEstoque);
+            this.Controls.Add(this.BotaoLançamentoPagamentos);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.BotaoLançamentoHorarios);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.BotaoCadastroProdutos);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.BotaoCadastroCliente);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.BotaoCadastroFuncionario);
+            this.Name = "Principal";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BotaoCadastroFuncionario;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button BotaoCadastroCliente;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button BotaoCadastroProdutos;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button BotaoLançamentoHorarios;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Button BotaoLançamentoPagamentos;
+        private System.Windows.Forms.Button BotaoLançamentoEstoque;
+        private System.Windows.Forms.Button BotaoGerarRelatorio;
+        private System.Windows.Forms.Button BotaoLançamentoServiços;
+    }
+}
+

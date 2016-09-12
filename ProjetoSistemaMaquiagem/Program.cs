f39ac90a8c91497c4501d65a66ff9517f457589f
diegohaji <diegohaji@gmail.com>
2016-09-12 17:15:44 -0300

    Classe login criada

--- ProjetoSistemaMaquiagem/Program.cs
+++ ProjetoSistemaMaquiagem/Program.cs
@@ -17,16 +17,16 @@ namespace ProjetoSistemaMaquiagem
         [STAThread]
         static void Main()
         {
-            //  Application.EnableVisualStyles();
-            //  Application.SetCompatibleTextRenderingDefault(false);
-            //  Application.Run(new TelaPrincipal());
+              Application.EnableVisualStyles();
+              Application.SetCompatibleTextRenderingDefault(false);
+              Application.Run(new TelaPrincipal());
 
-            ClnCliente cliente = new ClnCliente();
-            cliente.Cd_cliente = 666;
-            cliente.Nm_Cliente = "CAPIROSCO";
-            cliente.CPF_cliente = "10035965607";
-            cliente.Email_cliente = "capeta@paunoseucu.com";
-            cliente.Gravar();
+            //ClnCliente cliente = new ClnCliente();
+            //cliente.Cd_cliente = 666;
+            //cliente.Nm_Cliente = "CAPIROSCO";
+            //cliente.CPF_cliente = "10035965607";
+            //cliente.Email_cliente = "capeta@paunoseucu.com";
+            //cliente.Gravar();
         }
     }
 }

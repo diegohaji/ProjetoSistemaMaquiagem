c3437c30afab35f82887bc7dbdb4b85b7f12141b
Matheus Teles <matheus.teles221@outlook.com>
2016-09-14 07:53:07 -0300

    Classe login

--- CamadaDeNegocio/ClnLogin.cs
+++ CamadaDeNegocio/ClnLogin.cs
@@ -3,10 +3,32 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+using System.Data;
+using AcessoADados;
 
 namespace CamadaDeNegocio
 {
     class ClnLogin
-    {
+    {        
+        public bool validarLogin(string login, string senha)
+        {
+            string sql = "Select login, senha from tb_funcionario where login_funcionario="+login;
+            DataSet ds;
+            ClasseDados cd = new ClasseDados();
+            ds = cd.RetornarDataSet(sql);
+            if (ds.Tables[0].Rows.Count > 0)
+            {
+                Array dados = ds.Tables[0].Rows[0].ItemArray;
+
+            }
+            else
+            {
+                Console.WriteLine("Usuário ou senha inválidos, tente novamente");
+            }
+        }
+
+
+
     }
 }
